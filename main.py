fc0d63219def8028f670f599964cb2dc29b53077
julesjung <94782485+julesjung@users.noreply.github.com>
2026-01-31 19:00:25 +0100

    add title

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 import pygame
 
-screen = pygame.display.set_mode((640, 480), 0)
 
-while True:
-  for event in pygame.event.get():
-    if event.type == pygame.QUIT:
-      pygame.quit()
-      exit()
+class App:
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((640, 480), 0)
+        pygame.display.set_caption("PyGarden")
+        self.running = False
 
-  pygame.display.update()
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    return
+
+
+if __name__ == "__main__":
+    app = App()
+    app.run()
