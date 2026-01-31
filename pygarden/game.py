5e84e0ae614d7a5a6ce4a17a1d7d715ddbd6b113
julesjung <94782485+julesjung@users.noreply.github.com>
2026-02-01 00:12:26 +0100

    organize project

--- pygarden/game.py
+++ pygarden/game.py
@@ -1,7 +1,7 @@
 import pygame
 
 
-class App:
+class Game:
     def __init__(self):
         pygame.init()
         self.screen = pygame.display.set_mode((640, 480), 0)
@@ -12,10 +12,9 @@ class App:
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
-                    pygame.quit()
                     return
 
+            pygame.display.update()
 
-if __name__ == "__main__":
-    app = App()
-    app.run()
+    def quit(self):
+        pygame.quit()
