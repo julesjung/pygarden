1d88b949615f7d5225b2dfc468ca79e8af973bed
julesjung <94782485+julesjung@users.noreply.github.com>
2026-01-31 18:16:49 +0100

    fix linting errors

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 import pygame
-from pygame.locals import *
 
 screen = pygame.display.set_mode((640, 480), 0)
 
 while True:
   for event in pygame.event.get():
-    if event.type == QUIT:
+    if event.type == pygame.QUIT:
       pygame.quit()
       exit()
 
