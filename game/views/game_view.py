7865f5a04baa103af5a2dcb51377d24915b4e265
bymaxime <maximedasilva22100@gmail.com>
2026-02-01 12:43:58 +0100

    change background color

--- game/views/game_view.py
+++ game/views/game_view.py
@@ -5,7 +5,7 @@ class GameView(arcade.View):
     def __init__(self):
         super().__init__()
 
-        self.background_color = arcade.color.WHITE
+        self.background_color = arcade.color.CAMEL
 
     def on_draw(self):
         self.clear()
