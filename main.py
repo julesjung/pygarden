ffb43212b20f562761948d58e206987a05b10150
Jules Jung <94782485+julesjung@users.noreply.github.com>
2026-02-12 09:56:48 +0000

    use pickle for persistant storage

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import arcade
+""" import arcade
 
 from game.views.game_view import GameView
 from game.constants import WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE
@@ -8,8 +8,13 @@ def main():
     window = arcade.Window(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE)
     game = GameView()
     window.show_view(game)
-    window.run()
+    window.run() """
 
+import pickle
+
+def main():
+    with open("game_data.pkl", "rb") as data:
+        print(pickle.load(data))
 
 if __name__ == "__main__":
     main()
