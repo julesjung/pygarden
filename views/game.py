d2179e04bf8cd6d82d1885cd205ad38234ddd021
julesjung <94782485+julesjung@users.noreply.github.com>
2026-03-25 14:47:26 +0100

    add shop view

--- views/game.py
+++ views/game.py
@@ -2,9 +2,19 @@ import json
 import time
 
 import arcade
+import arcade.gui
+import arcade.gui.experimental
 
 from data import get_save_file
 from music import MusicPlayer
+from plants_properties import plants
+
+BUTTON_STYLE = {
+    "normal": arcade.gui.UITextureButton.UIStyle(font_name="Do Hyeon"),
+    "hover": arcade.gui.UITextureButton.UIStyle(font_name="Do Hyeon"),
+    "press": arcade.gui.UITextureButton.UIStyle(font_name="Do Hyeon"),
+    "disabled": arcade.gui.UITextureButton.UIStyle(font_name="Do Hyeon"),
+}
 
 
 class GameView(arcade.View):
@@ -60,17 +70,41 @@ class GameView(arcade.View):
 
         self.overlay_sprites = arcade.SpriteList()
 
-        """
-        self.shop_view = ShopView(self.window)
         self.in_shop = False
+        self.shop_background_sprites = arcade.SpriteList()
+        self.shop_background = arcade.BasicSprite(
+            arcade.load_texture("assets/shop/background.png"),
+            center_x=512,
+            center_y=384,
+        )
+        self.shop_background_sprites.append(self.shop_background)
 
-        @self.shop.event
-        def on_mouse_press(x, y, buttons, modifiers):
-            self.on_shop_press(x, y, buttons, modifiers)
+        self.shop_manager = arcade.gui.UIManager()
 
-        self.save_game()
-        self.leaf_counter = LeafCounter(self.data["leaf_count"], 0, 0)
-        """
+    def on_show_view(self) -> None:
+        shop_scroll_area = arcade.gui.experimental.UIScrollArea(
+            x=130,
+            y=89,
+            width=350,
+            height=371,
+        )
+        shop_tree_list = arcade.gui.UIBoxLayout(space_between=16)
+
+        for plant in plants:
+            plant_widget = arcade.gui.UITextureButton(
+                width=350,
+                height=48,
+                texture=arcade.load_texture("assets/shop/frame.png"),
+                text=plant["name"],
+                style=BUTTON_STYLE,
+            )
+            shop_tree_list.add(plant_widget)
+
+        shop_scroll_area.add(shop_tree_list)
+        shop_scroll_area.scroll_speed = 16
+        shop_scroll_area.invert_scroll = True
+
+        self.shop_manager.add(shop_scroll_area)
 
     def on_draw(self):
         self.clear()
@@ -81,40 +115,45 @@ class GameView(arcade.View):
 
         self.overlay_sprites.draw()
 
-        """
-        self.leaf_counter.draw()
-
         if self.in_shop:
-            self.shop_view.draw()
-        """
+            arcade.draw_lbwh_rectangle_filled(0, 0, 1024, 768, (0, 0, 0, 192))
+            self.shop_background_sprites.draw()
+            self.shop_manager.draw()
 
     def on_mouse_drag(
         self, x: int, y: int, dx: int, dy: int, _buttons: int, _modifiers: int
     ):
-        self.camera.position = (
-            max(512, min(self.camera.position.x - dx, 1536)),
-            max(384, min(self.camera.position.y - dy, 1152)),
-        )
+        if not self.in_shop:
+            self.camera.position = (
+                max(512, min(self.camera.position.x - dx, 1536)),
+                max(384, min(self.camera.position.y - dy, 1152)),
+            )
 
     def on_mouse_motion(self, x, y, dx, dy):
-        (world_x, world_y, _) = self.camera.unproject((x, y))
-        hovered = None
-        for sprite in self.foregroud_sprites:
-            if sprite.collides_with_point((world_x, world_y)):
-                hovered = sprite
-                break
-        if hovered != self.hovered:
-            if self.hovered is not None:
-                self.hovered.color = (255, 255, 255)
-            if hovered is not None:
-                hovered.color = (255, 200, 200)
-            self.hovered = hovered
+        if not self.in_shop:
+            (world_x, world_y, _) = self.camera.unproject((x, y))
+            hovered = None
+            for sprite in self.foregroud_sprites:
+                if sprite.collides_with_point((world_x, world_y)):
+                    hovered = sprite
+                    break
+            if hovered != self.hovered:
+                if self.hovered is not None:
+                    self.hovered.color = (255, 255, 255)
+                if hovered is not None:
+                    hovered.color = (255, 200, 200)
+                self.hovered = hovered
 
     def on_mouse_press(self, x, y, button, modifiers):
-        (world_x, world_y, _) = self.camera.unproject((x, y))
-        for sprite in self.foregroud_sprites:
-            if sprite.collides_with_point((world_x, world_y)):
-                break
+        if not self.in_shop:
+            (world_x, world_y, _) = self.camera.unproject((x, y))
+            if self.shop.collides_with_point((world_x, world_y)):
+                self.in_shop = True
+                self.shop_manager.enable()
+        else:
+            if not self.shop_background.collides_with_point((x, y)):
+                self.in_shop = False
+                self.shop_manager.disable()
 
     def save_game(self):
         self.data["last_played"] = time.time()
