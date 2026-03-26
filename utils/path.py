a7333a5d70b088f0d3c2671af4bdcce38612118e
Jules Jung <94782485+julesjung@users.noreply.github.com>
2026-03-26 09:23:55 +0000

    change path config to allow for executables

--- utils/path.py
+++ utils/path.py
@@ -1,3 +1,7 @@
 from pathlib import Path
+import sys
 
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+if getattr(sys, 'frozen', False) and hasattr(sys, '_MEIPASS'):
+    BASE_DIR = Path(sys.executable).resolve().parent
