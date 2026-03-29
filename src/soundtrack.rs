c00a2dd6c2434740548addb7e5293175ebe693fc
julesjung <94782485+julesjung@users.noreply.github.com>
2026-03-29 18:37:08 +0200

    fix soundtrack starting twice

--- src/soundtrack.rs
+++ src/soundtrack.rs
@@ -4,21 +4,23 @@ use rand::{prelude::*, rng};
 #[derive(Resource)]
 pub struct SoundtrackPlayer {
     track_list: Vec<Handle<AudioSource>>,
-    current: usize,
+    current_index: usize,
+    current_entity: Option<Entity>,
 }
 
 impl SoundtrackPlayer {
     fn new(track_list: Vec<Handle<AudioSource>>) -> Self {
         Self {
             track_list,
-            current: 0,
+            current_index: 0,
+            current_entity: None,
         }
     }
 
     fn next(&mut self) -> Handle<AudioSource> {
-        self.current = (self.current + 1) % self.track_list.len();
+        self.current_index = (self.current_index + 1) % self.track_list.len();
 
-        self.track_list[self.current].clone()
+        self.track_list[self.current_index].clone()
     }
 }
 
@@ -32,21 +34,35 @@ pub fn setup_soundtrack(mut commands: Commands, asset_server: Res<AssetServer>) 
 
     track_list.shuffle(&mut rng());
 
-    commands.insert_resource(SoundtrackPlayer::new(track_list));
+    let mut soundtrack = SoundtrackPlayer::new(track_list);
+
+    let entity = commands.spawn((
+        AudioPlayer(soundtrack.track_list[soundtrack.current_index].clone()),
+        PlaybackSettings {
+            mode: PlaybackMode::Despawn,
+            ..Default::default()
+        },
+    ));
+    soundtrack.current_entity = Some(entity.id());
+
+    commands.insert_resource(soundtrack);
 }
 
 pub fn update_soundtrack(
     mut commands: Commands,
-    query: Query<Entity, With<AudioSink>>,
+    query: Query<(), With<AudioPlayer>>,
     mut soundtrack: ResMut<SoundtrackPlayer>,
 ) {
-    if query.is_empty() {
-        commands.spawn((
+    if let Some(current_entity) = soundtrack.current_entity
+        && query.get(current_entity).is_err()
+    {
+        let entity = commands.spawn((
             AudioPlayer(soundtrack.next()),
             PlaybackSettings {
-                mode: PlaybackMode::Once,
+                mode: PlaybackMode::Despawn,
                 ..Default::default()
             },
         ));
+        soundtrack.current_entity = Some(entity.id());
     }
 }
