ce9692fb4eab9bd1c5e63b8225eb399d657b0887
julesjung <94782485+julesjung@users.noreply.github.com>
2026-03-30 20:32:46 +0200

    create godot project

---
+++ scripts/player.gd
@@ -0,0 +1,5 @@
+extends Node2D
+
+func _process(delta: float) -> void:
+	var input_direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	global_position += input_direction * 5
--- utils/path.py
+++
@@ -1,8 +0,0 @@
-from pathlib import Path
-import sys
-
-BASE_DATA_DIR = Path(__file__).resolve().parent.parent
-BASE_ASSETS_DIR = Path(__file__).resolve().parent.parent
-
-if getattr(sys, "frozen", False) and hasattr(sys, "_MEIPASS"):
-    BASE_DATA_DIR = Path(sys.executable).resolve().parent
