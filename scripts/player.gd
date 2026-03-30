68c01f2361891a1f19c310321d1b484f98a5472f
julesjung <94782485+julesjung@users.noreply.github.com>
2026-03-30 21:31:01 +0200

    add soundtrack player

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,4 +2,4 @@ extends Node2D
 
 func _process(delta: float) -> void:
 	var input_direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	global_position += input_direction * 5
+	global_position += input_direction * 256 * delta
---
+++ scripts/soundtrack.gd
@@ -0,0 +1,23 @@
+extends Node
+
+@export var tracks: Array[AudioStream] = []
+var _current_index: int = 0
+
+var _player: AudioStreamPlayer
+
+func _ready() -> void:
+	_player = AudioStreamPlayer.new()
+	add_child(self._player)
+	
+	tracks.shuffle()
+	_play()
+	
+	_player.finished.connect(_finished)
+	
+func _play() -> void:
+	_player.stream = tracks[_current_index]
+	_player.play()
+
+func _finished() -> void:
+	_current_index = (_current_index + 1) % len(tracks)
+	_play()
