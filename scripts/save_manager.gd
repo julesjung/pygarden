39ac770ad9ad42d6e48ffd5797d80ae5ddbebd78
julesjung <94782485+julesjung@users.noreply.github.com>
2026-05-28 22:40:07 +0200

    feat(data): save in binary format for faster game loading

--- scripts/save_manager.gd
+++ scripts/save_manager.gd
@@ -1,6 +1,6 @@
 extends Node
 
-const SAVE_PATH = "user://game_data.json"
+const SAVE_PATH = "user://game_data.bin"
 const DEFAULT_DATA = {
 	"leaves": 100,
 	"plants": []
@@ -11,19 +11,17 @@ func load_game() -> Dictionary:
 		return DEFAULT_DATA
 
 	var file = FileAccess.open(SAVE_PATH, FileAccess.READ)
-	var content = file.get_as_text()
-	return JSON.parse_string(content)
+	var data = file.get_buffer(file.get_length())
+	return bytes_to_var(data)
 
 func save_game(plants: Array):
 	var plants_data = []
 	for plant in plants:
 		plants_data.append(plant.save())
-	var data = JSON.stringify({
+	var data = var_to_bytes({
 		"leaves": 100,
 		"plants": plants_data
 	})
 	
 	var file = FileAccess.open(SAVE_PATH, FileAccess.WRITE)
-	file.store_line(data)
-	
-	
+	file.store_buffer(data)
